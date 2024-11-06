14d9f5b4636da8e248bf09c6ea559c746369d536
eddu_dev <eddu17g@gmail.com>
2024-11-06 00:55:39 -0500

    changed doctors and contact us

--- js/map-active.js
+++ js/map-active.js
@@ -1,80 +1,81 @@
 $(document).ready(function(){
-/*====================================
-	Google Map JS
-======================================*/ 	
-(function(){
-
-	var map;
-	map = new GMaps({
-		el: '#myMap',
-		lat: 51.507351,
-		lng: -0.127758,
-		scrollwheel:false,
-		zoom: 15,
-		zoomControl : false,
-		panControl : false,
-		streetViewControl : true,
-		mapTypeControl: false,
-		overviewMapControl: false,
-		clickable: false
+	/*====================================
+		Google Map JS
+	======================================*/ 	
+	(function(){
+	
+		var map;
+		map = new GMaps({
+			el: '#myMap',
+			lat: -0.2975031,
+			lng: -78.5497131,
+			scrollwheel: false,
+			zoom: 15,
+			zoomControl: false,
+			panControl: false,
+			streetViewControl: true,
+			mapTypeControl: false,
+			overviewMapControl: false,
+			clickable: false
+		});
+	
+		var image = 'img/map-marker.png';
+		map.addMarker({
+			lat: -0.2975031,
+			lng: -78.5497131,
+			icon: image,
+			animation: google.maps.Animation.DROP,
+			verticalAlign: 'bottom',
+			horizontalAlign: 'left',
+			backgroundColor: '#efece0',
+		});
+	
+		var styles = [
+	
+			{
+				"featureType": "road",
+				"stylers": [
+					{ "color": "#ffffff" }
+				]
+			}, {
+				"featureType": "water",
+				"stylers": [
+					{ "color": "#bde5f6" }
+				]
+			}, {
+				"featureType": "landscape",
+				"stylers": [
+					{ "color": "#f2f2f2" }
+				]
+			}, {
+				"elementType": "labels.text.fill",
+				"stylers": [
+					{ "color": "#FF7550" }
+				]
+			}, {
+				"featureType": "poi",
+				"stylers": [
+					{ "color": "#e2f0cd" }
+				]
+			}, {
+				"elementType": "labels.text",
+				"stylers": [
+					{ "saturation": 2 },
+					{ "weight": 0.3 },
+					{ "color": "#a8a8a8" }
+				]
+			}
+	
+		];
+	
+		map.addStyle({
+			styledMapName: "Styled Map",
+			styles: styles,
+			mapTypeId: "map_style"
+		});
+	
+		map.setStyle("map_style");
+	}());
+	
 	});
-
-	var image = 'img/map-marker.png';
-	map.addMarker({
-		lat: 51.507351,
-		lng: -0.127758,
-		icon: image,
-		animation: google.maps.Animation.DROP,
-		verticalAlign: 'bottom',
-		horizontalAlign: 'left',
-		backgroundColor: '#efece0',
-	});
-
-	var styles = [
-
-		{
-			"featureType": "road",
-			"stylers": [
-				{ "color": "#ffffff" }
-			]
-		},{
-			"featureType": "water",
-			"stylers": [
-				{ "color": "#bde5f6" }
-			]
-		 },{
-		  "featureType": "landscape",
-			 "stylers": [
-			 { "color": "#f2f2f2" }
-			 ]
-		},{
-			"elementType": "labels.text.fill",
-			"stylers": [
-				{ "color": "#FF7550" }
-			]
-		},{
-			"featureType": "poi",
-			"stylers": [
-			 { "color": "#e2f0cd" }
-			]
-		},{
-			"elementType": "labels.text",
-			"stylers": [
-				{ "saturation": 2 },
-				{ "weight": 0.3},
-				{ "color": "#a8a8a8" }
-			]
-		}
-
-	];
-
-	 map.addStyle({
-		styledMapName:"Styled Map",
-		styles: styles,
-		mapTypeId: "map_style"
-	});
-
-	map.setStyle("map_style");
-}());
-
-});
+	
